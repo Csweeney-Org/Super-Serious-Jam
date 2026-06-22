78561d30b6997846742eb9df35cf14240b01e387
curtis <curtis.addinall@gmail.com>
2026-06-22 23:41:18 +0800

    projectile pooling and pickup items

---
+++ Assets/Scripts/Throwables/ItemPickup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Throwables
+{
+    public class ItemPickup : MonoBehaviour
+    {
+        public BoingData BoingProperties = new BoingData(5, 3, 5, true);
+        public MeshFilter Mesh;
+        public Collider PickupCollider;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            var collidingUnit = other.GetComponentInChildren<ThrowableInventory>();
+            if (collidingUnit == null) return;
+
+            if (collidingUnit.TryPickupItem(this))
+            {
+                PickupCollider.enabled = false;
+            }
+        }
+
+        public void OnValidate()
+        {
+            PickupCollider ??= GetComponent<Collider>();
+            if (PickupCollider == null || !PickupCollider.isTrigger)
+            {
+                Debug.LogError($"Expected to find a trigger collider for ItemPickup {this.name}");
+            }
+            Mesh ??= GetComponent<MeshFilter>();
+            if (Mesh == null)
+            {
+                Debug.LogError($"Expected to find a mesh filterr for ItemPickup {this.name}");
+            }
+        }
+    }
+}
